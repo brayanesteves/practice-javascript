2f257d0f3cf09c9f0fb3923102212e727e4b38be
Brayan Esteves <brayan.esteves93@gmail.com>
2023-05-29 15:53:53 -0400

    QuerySelector [29.05.2023]

--- views/eyes/eyes-refactor/assets/js/eyes.js
+++ views/eyes/eyes-refactor/assets/js/eyes.js
@@ -1,8 +1,8 @@
-const balls = document.getElementsByClassName("ball");
+const balls = document.querySelectorAll(".ball");
 
 document.addEventListener('mousemove', function (e) {
-    let x = (e.clientX * 100) / window.innerWidth  + "%";
-    let y = (e.clientY * 100) / window.innerHeight + "%";
+    const x = (e.clientX * 100) / window.innerWidth  + "%";
+    const y = (e.clientY * 100) / window.innerHeight + "%";
     for(var i = 0;i < 2; i++) {
         balls[i].style.left      = x;
         balls[i].style.top       = y;
