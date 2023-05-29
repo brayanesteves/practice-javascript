26f015e0f328e931a9baba393b43fe259a1dbad5
Brayan Esteves <brayan.esteves93@gmail.com>
2023-05-29 15:46:16 -0400

    Convert JS to EcmaScript [29.05.2023]

--- views/eyes/eyes-refactor/assets/js/eyes.js
+++ views/eyes/eyes-refactor/assets/js/eyes.js
@@ -1,12 +1,12 @@
-var balls            = document.getElementsByClassName("ball");
+const balls = document.getElementsByClassName("ball");
 
-document.onmousemove = function(e) {
-    var x = (e.clientX * 100) / window.innerWidth  + "%";
-    var y = (e.clientY * 100) / window.innerHeight + "%";
+document.addEventListener('mousemove', function (e) {
+    let x = (e.clientX * 100) / window.innerWidth  + "%";
+    let y = (e.clientY * 100) / window.innerHeight + "%";
     for(var i = 0;i < 2; i++) {
         balls[i].style.left      = x;
         balls[i].style.top       = y;
         balls[i].style.transform = "traslate(-" + x + ",-" + y + ")";
     }
     
-}
+});
