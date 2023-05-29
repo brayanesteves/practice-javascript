28dd005d633f9dd696720480b6dff235d02587a7
Brayan Esteves <brayan.esteves93@gmail.com>
2023-05-29 17:58:50 -0400

    Try and Catch [29.05.2023]

--- views/eyes/eyes-refactor/assets/js/eyes.js
+++ views/eyes/eyes-refactor/assets/js/eyes.js
@@ -1,12 +1,16 @@
-const balls = document.querySelectorAll(".ball");
-
-document.addEventListener('mousemove', (event) => {
-    const x = (event.clientX * 100) / window.innerWidth  + "%";
-    const y = (event.clientY * 100) / window.innerHeight + "%";
-    balls.forEach((ball) => {
-        ball.style.left      = x;
-        ball.style.top       = y;
-        ball.style.transform = "traslate(-" + x + ",-" + y + ")";
-    })
+try {    
+    const balls = document.querySelectorAll(".ball");
     
-});
+    document.addEventListener('mousemove', (event) => {
+        const x = (event.clientX * 100) / window.innerWidth  + "%";
+        const y = (event.clientY * 100) / window.innerHeight + "%";
+        balls.forEach((ball) => {
+            ball.style.left      = x;
+            ball.style.top       = y;
+            ball.style.transform = "traslate(-" + x + ",-" + y + ")";
+        })
+        
+    });
+} catch(err) {
+    console.error(err);
+}
