e88a05079c0e4d6fb495b4a18dea0efff26ce470
Brayan Esteves <brayan.esteves93@gmail.com>
2023-05-29 16:01:31 -0400

    Fat Arrow Function with 'forEach' [29.05.2023]

--- views/eyes/eyes-refactor/assets/js/eyes.js
+++ views/eyes/eyes-refactor/assets/js/eyes.js
@@ -1,12 +1,12 @@
 const balls = document.querySelectorAll(".ball");
 
-document.addEventListener('mousemove', function (e) {
-    const x = (e.clientX * 100) / window.innerWidth  + "%";
-    const y = (e.clientY * 100) / window.innerHeight + "%";
-    for(var i = 0;i < 2; i++) {
-        balls[i].style.left      = x;
-        balls[i].style.top       = y;
-        balls[i].style.transform = "traslate(-" + x + ",-" + y + ")";
-    }
+document.addEventListener('mousemove', (event) => {
+    const x = (event.clientX * 100) / window.innerWidth  + "%";
+    const y = (event.clientY * 100) / window.innerHeight + "%";
+    balls.forEach((ball) => {
+        ball.style.left      = x;
+        ball.style.top       = y;
+        ball.style.transform = "traslate(-" + x + ",-" + y + ")";
+    })
     
 });
