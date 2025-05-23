ee71e59d6d582f5bc415c62a9abf80c43d07b36e
Brayan Esteves <brayan.esteves93@gmail.com>
2025-05-23 14:54:39 -0400

    [Youtube][jonmircha] 01. Data types - 01. Const -> 'objects-and-arrays-with-let.js' [23/05/2025]

---
+++ views/youtube/jonmircha/01.data-types/01.const/assets/js/objects-and-arrays-with-let.js
@@ -0,0 +1,14 @@
+let objeto = {
+    nombre:"Jon",
+    edad:35
+};
+console.log(objeto);
+
+let colores = ["blanco", "negro", "azul"];
+console.log(colores);
+
+objeto.correo = "jonmircha@gmail.com";
+console.log(objeto);
+
+colores.push("anaranjado");
+console.log(colores);
