883e39ec04648f09d401de3314786a8f81e587f3
Brayan Esteves <brayan.esteves93@gmail.com>
2025-05-23 14:54:12 -0400

    [Youtube][jonmircha] 01. Data types - 01. Const -> 'objects-and-arrays-with-const.js' [23/05/2025]

---
+++ views/youtube/jonmircha/01.data-types/01.const/assets/js/objects-and-arrays-with-const.js
@@ -0,0 +1,14 @@
+const objeto = {
+    nombre:"Jon",
+    edad:35
+};
+console.log(objeto);
+
+const colores = ["blanco", "negro", "azul"];
+console.log(colores);
+
+objeto.correo = "jonmircha@gmail.com";
+console.log(objeto);
+
+colores.push("anaranjado");
+console.log(colores);
