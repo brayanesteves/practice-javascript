342e66ab4d229ef4690086ffce8a075de4c7f4e7
Brayan Esteves <brayan.esteves93@gmail.com>
2025-05-25 19:46:18 -0400

    [Youtube][jonmircha] 01. Data types - 05. Undefined, null & NaN [25/05/2025]

---
+++ views/youtube/jonmircha/01.data-types/06.undefined-null-&-nan/assets/js/main.js
@@ -0,0 +1,29 @@
+/**
+ * [ENGLISH]
+ * 
+ * 
+ * [SPANISH]
+ * 'undefined' indica que no se ha inicializado una variable y que el valor está ausente.
+ */
+let indefinida;
+console.log(indefinida); // undefined
+
+/**
+ * [ENGLISH]
+ * 
+ * 
+ * [SPANISH]
+ * 'null' es un valor especial que indica la ausencia de un valor.
+ */
+let nulo = null;
+console.log(nulo); // null
+
+/**
+ * [ENGLISH]
+ * 
+ * 
+ * [SPANISH]
+ * NaN - Not a Number
+ */
+let noEsUnNumero = "hola" * 3.27;
+console.log(noEsUnNumero); // NaN
