93d113ff9e1f7e476f794c5e924f01e93732251d
Brayan Esteves <brayan.esteves93@gmail.com>
2025-11-05 07:44:00 -0400

    [Youtube][jonmircha] 04. Objects and Functions of Language - 03. Math object [05/11/2025]

---
+++ views/youtube/jonmircha/04.objects-and-functions-of-language/02.math-object/assets/js/main.js
@@ -0,0 +1,21 @@
+console.log(Math); // Output: Math {abs: f, acos: f, acosh: f, asin: f, asinh: f, ...}
+
+console.log(Math.PI); // Output: 3.141592653589793
+console.log(Math.abs(7.8)); // Output: 7.8
+console.log(Math.abs(-7.8)); // Output: 7.8
+console.log(Math.ceil(7.8)); // Output: 8
+console.log(Math.ceil(7.2)); // Output: 8
+console.log(Math.floor(7.8)); // Output: 7
+console.log(Math.round(7.2)); // Output: 7
+console.log(Math.round(7.8)); // Output: 8
+console.log(Math.round(7.5)); // Output: 8
+console.log(Math.round(7.49)); // Output: 7
+console.log(Math.sqrt(81)); // Output: 9
+console.log(Math.pow(2, 5)); // Output: 32
+console.log(Math.sign(-7.8)); // Output: -1
+console.log(Math.sign(-5)); // Output: -1
+console.log(Math.sign(6.5)); // Output: 1
+console.log(Math.sign(0)); // Output: 0
+console.log(Math.random()); // Output: 0.2459674312136091
+console.log(Math.random() * 1000); // Output: 712.0941328740522
+console.log(Math.round(Math.random() * 1000)); // Output: 166
