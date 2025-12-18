1a70bdd40978d81a5d1aadb917f3ade787de126c
Brayan Esteves <brayan.esteves93@gmail.com>
2025-12-17 23:50:35 -0400

    [Youtube][jonmircha] 11. DOM: Practical Exercises - 08. Dark/Light Theme with localStorage [17/12/2025]

---
+++ views/youtube/jonmircha/11.dom-practical-exercises/08.dark-light-theme-with-localstorage/assets/js/dom/boton_scroll.js
@@ -0,0 +1,27 @@
+const d = document;
+const w = window;
+
+export default function scrollTopButton(btn) {
+    const $scrollBtn = d.querySelector(btn);
+
+    w.addEventListener("scroll", (e) => {
+        let scrollTop = w.pageYOffset || d.documentElement.scrollTop;
+
+        if (scrollTop > 600) {
+            $scrollBtn.classList.remove("hidden");
+        } else {
+            $scrollBtn.classList.add("hidden");
+        }
+
+        console.log(w.pageYOffset, d.documentElement.scrollTop); // For Example. Output: 183 183
+    });
+
+    d.addEventListener("click", (e) => {
+        if (e.target.matches(btn)) {
+            w.scrollTo({
+                 top: 0,
+                // left: 0,
+            });
+        }
+    });
+}
---
+++ views/youtube/jonmircha/11.dom-practical-exercises/08.dark-light-theme-with-localstorage/assets/js/index_dom.js
@@ -0,0 +1,32 @@
+import   hamburgerMenu         from "./dom/menu_hamburguesa.js";
+import { digitalClock, alarm } from "./dom/reloj.js";
+import { moveBall, shortcuts } from "./dom/teclado.js";
+import { countdown }           from "./dom/cuenta_regresiva.js";
+import   scrollTopButton       from "./dom/boton_scroll.js";
+import   darkTheme             from "./dom/tema_oscuro.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+    hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+    alarm("assets/sounds/mp3/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+    // countdown("countdown", "May 23, 2021 03:23:19", "Feliz Cumpleaños amigo y docente digital 🤓");
+    countdown("countdown", "May 23, 2020 16:17:19", "Feliz Cumpleaños amigo y docente digital 🤓");
+    scrollTopButton(".scroll-top-btn");
+});
+
+d.addEventListener("keydown", (e) => {
+    shortcuts(e);
+    moveBall(e, ".ball", ".stage");
+});
+
+d.addEventListener("keyup", (e) => {
+    shortcuts(e);
+});
+
+d.addEventListener("keypress", (e) => {
+    shortcuts(e);
+});
+
+darkTheme(".dark-theme-btn", "dark-mode");
