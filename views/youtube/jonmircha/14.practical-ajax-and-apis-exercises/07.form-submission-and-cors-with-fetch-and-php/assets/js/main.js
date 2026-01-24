717dbfaaacf9e9f4e71d575f3e48b4a0f6040010
Brayan Esteves <brayan.esteves93@gmail.com>
2026-01-24 19:33:44 -0400

    [Youtube][jonmircha] 14. Practical AJAX and API's Exercises - 07. Form Submission and CORS with Fetch and PHP [24/01/2026]

---
+++ views/youtube/jonmircha/14.practical-ajax-and-apis-exercises/07.form-submission-and-cors-with-fetch-and-php/assets/js/main.js
@@ -0,0 +1,92 @@
+const d = document;
+
+function contactForm() {
+    const $form   = d.querySelector(".contact-form");
+    const $inputs = d.querySelectorAll(".contact-form [required]");
+
+    // console.log($inputs); // Output: NodeList(4) [input, input, input, textarea]
+
+    $inputs.forEach((input) => {
+        const $span = d.createElement("span");
+
+        $span.id          = input.name;
+        $span.textContent = input.title;
+
+        $span.classList.add("contact-form-error", "none");
+        input.insertAdjacentElement("afterend", $span);
+    });
+
+    d.addEventListener("keyup", (e) => {
+        if (e.target.matches(".contact-form [required]")) {
+            let $input  = e.target;
+            let pattern = $input.pattern || $input.dataset.pattern;
+            
+            // console.log($input, pattern); // For Example. Output: <input type="text" name="name" placeholder="Escibe tu nombre" title="El nombre sólo acepta letras y espacios en blanco" pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$" required /> "^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$"
+            
+            if(pattern && $input.value !== "") {
+                // console.log("El 'input' tiene patrón.");
+                let regex = new RegExp(pattern);
+                return !regex.exec($input.value) ? d.getElementById($input.name).classList.add("is-active") : d.getElementById($input.name).classList.remove("is-active");
+            }
+
+            if(!pattern) {
+                //console.log("El 'input' NO tiene patrón.");
+                return $input.value === "" ? d.getElementById($input.name).classList.add("is-active") : d.getElementById($input.name).classList.remove("is-active");
+            }
+        }
+    });
+
+    d.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Enviando Formulario.");
+
+        const $loader   = d.querySelector(".contact-form-loader");
+        const $response = d.querySelector(".contact-form-response");
+
+        $loader.classList.remove("none");
+
+        // <LOCAL> //
+        fetch("../php/server/send_mail.php", {
+            method: "POST",
+            body: new FormData(e.target)
+        }).then((res) => res.ok ? res.json() : Promise.reject(res)).then((json) => {
+            // console.log(json); // For Example. Output: {success: "true", message: "The form was submitted successfully."}
+            $loader.classList.add("none");
+            $response.classList.remove("none");
+            $response.innerHTML = `<p>${json.message}</p>`;
+            $form.reset();
+        }).catch((err) => {
+            // console.log(err); // For Example. Output: 
+            let message = err.statusText || "Ocurrio un error al enviar, intenta nuevamente.";
+            $response.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
+        }).finally(() => setTimeout(() => {
+            $response.classList.add("none");
+            $response.innerHTML = "";
+        }, 3000));
+        // <.LOCAL> //
+
+        // <SERVER> //
+        fetch("https://jonmircha.com/send_mail.php", {
+            method: "POST",
+            body: new FormData(e.target),
+            mode: "cors"
+        }).then((res) => res.ok ? res.json() : Promise.reject(res)).then((json) => {
+            // console.log(json); // For Example. Output: {success: "true", message: "The form was submitted successfully."}
+            $loader.classList.add("none");
+            $response.classList.remove("none");
+            $response.innerHTML = `<p>${json.message}</p>`;
+            $form.reset();
+        }).catch((err) => {
+            // console.log(err); // For Example. Output: 
+            let message = err.statusText || "Ocurrio un error al enviar, intenta nuevamente.";
+            $response.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
+        }).finally(() => setTimeout(() => {
+            $response.classList.add("none");
+            $response.innerHTML = "";
+        }, 3000));
+        // <.SERVER> //
+
+    });
+}
+
+d.addEventListener("DOMContentLoaded", contactForm);
