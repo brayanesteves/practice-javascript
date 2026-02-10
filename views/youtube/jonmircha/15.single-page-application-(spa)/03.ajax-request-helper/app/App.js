f01456c3af5135aaf53842f479403d7bf6c8390a
Brayan Esteves <brayan.esteves93@gmail.com>
2026-02-10 17:45:05 -0400

    [Youtube][jonmircha] 15. Single Page Application (SPA) - 03. AJAX Request Helper [10/02/2026]

---
+++ views/youtube/jonmircha/15.single-page-application-(spa)/03.ajax-request-helper/app/App.js
@@ -0,0 +1,15 @@
+import   api    from "./helpers/wp_api.js";
+import { ajax } from "./helpers/ajax.js";
+
+export function App() {
+    document.getElementById("root").innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla JS</h1>`;
+
+    // console.log(api); // For Example. Output: {NAME: "malvestida", DOMAIN: "https://malvestida.com", SITE:"https://malvestida.com/wp-json", API_WP: "https://malvestida.com/wp-json/v2", POSTS: "https://malvestida.com/wp-json/wp/v2/posts?_embed", ...}
+
+    ajax({ 
+        url:api.POSTS_EMBED,
+        cbSuccess:(posts) => {
+            console.log(posts); // For Example. Output: (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+        },
+    });
+}
