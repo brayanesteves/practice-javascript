6bd33c6f0d311ca3fb682d1b5b1ebd8c4bb29b8b
Brayan Esteves <brayan.esteves93@gmail.com>
2026-02-11 12:18:52 -0400

    [Youtube][jonmircha] 15. Single Page Application (SPA) - 06. Dynamic Rendering [11/02/2026]

---
+++ views/youtube/jonmircha/15.single-page-application-(spa)/06.dynamic-rendering/app/helpers/ajax.js
@@ -0,0 +1,14 @@
+export function ajax(props) {
+    let { url, cbSuccess } = props;
+
+    fetch(url).then((res) => res.ok ? res.json() : Promise.reject(res)).then((json) => cbSuccess(json)).catch((err) => {
+        let message = err.statusText || "Ocurrio un error al acceder a la API.";
+        document.getElementById("root").innerHTML = `
+            <div class="error>
+                <p>Error ${err.status}: ${message}</p>
+            </div>
+        `;
+
+        console.log(err); // For Example. Output: Response {type: 'basic', url: 'http://127.0.0.1:5501/views/youtube/jonmircha/15.s…pplication-(spa)/04.first-ui-components/no-valida', redirected: false, status: 404, ok: false, …}
+    });
+}
