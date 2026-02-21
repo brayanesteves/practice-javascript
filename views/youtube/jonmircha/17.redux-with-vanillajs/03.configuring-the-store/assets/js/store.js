32033533fd8ea5f62d662f0c73aa98be6d417589
Brayan Esteves <brayan.esteves93@gmail.com>
2026-02-21 08:00:36 -0400

    [Youtube][jonmircha] 17. Redux with VanillaJS - 02. Configuring the Store [21/02/2026]

---
+++ views/youtube/jonmircha/17.redux-with-vanillajs/03.configuring-the-store/assets/js/store.js
@@ -0,0 +1,53 @@
+import * as Redux from "https://unpkg.com/redux@latest/dist/redux.browser.mjs";
+
+/**
+ * [ENGLISH]
+ * State Initial and Global of the App.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Estado Inicial y Globlal de la App.
+ */
+const initialState = {
+    counter: 0,
+};
+
+/**
+ * [ENGLISH]
+ * Define the types of actions of the App.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Definimos los tipos de acciones de la App.
+ */
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
+/**
+ * [ENGLISH]
+ * Reducer: A pure function that handles state changes based on actions.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Reducer: Una función pura que gestiona los cambios en el estado en función de las acciones.
+ */
+function counterReducer(state = initialState, action) {
+
+}
+
+/**
+ * [ENGLISH]
+ * Create the 'Store'.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Creamos la 'Store'.
+ */
+const store = Redux.createStore(counterReducer);
+
+export default store;
