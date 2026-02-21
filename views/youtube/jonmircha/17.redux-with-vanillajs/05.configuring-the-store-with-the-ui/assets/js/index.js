4c0fb0618d19367b9e8f3542eebe2b9eb3e11c70
Brayan Esteves <brayan.esteves93@gmail.com>
2026-02-21 18:49:26 -0400

    [Youtube][jonmircha] 17. Redux with VanillaJS - 05. Configuring the Store the UI [21/02/2026]

---
+++ views/youtube/jonmircha/17.redux-with-vanillajs/05.configuring-the-store-with-the-ui/assets/js/index.js
@@ -0,0 +1,45 @@
+import store from "./store.js";
+
+const d = document;
+
+const $counterValue = d.getElementById("counter-value");
+const $incrementBtn = d.getElementById("increment-btn");
+const $decrementBtn = d.getElementById("decrement-btn");
+const $resetBtn     = d.getElementById("reset-btn");
+
+/**
+ * [ENGLISH]
+ * Function to update the displayed value of the counter in the DOM.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Función para actualizar el valor mostrado del contador en el DOM.
+ */
+function render() {
+    const state = store.getState();
+    $counterValue.textContent = state.counter;
+}
+
+/**
+ * [ENGLISH]
+ * Subscribe the 'render' function to be executed every time the state changes.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Suscribammos la función 'render' para que se ejecute cada vez que el estado cambie.
+ */
+store.subscribe(render);
+
+/**
+ * [ENGLISH]
+ * Render the initial value of the counter.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Renderizamos el valor inicial del contador.
+ */
+// render();
+d.addEventListener("DOMContentLoaded", render);
---
+++ views/youtube/jonmircha/17.redux-with-vanillajs/05.configuring-the-store-with-the-ui/assets/js/store.js
@@ -0,0 +1,71 @@
+import * as Redux from "https://unpkg.com/redux@latest/dist/redux.browser.mjs";
+
+/**
+ * [ENGLISH]
+ * State Initial and Global of the App.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Estado Inicial y Globlal de la App.
+ */
+const initialState = {
+    counter: 0,
+};
+
+/**
+ * [ENGLISH]
+ * Define the types of actions of the App.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Definimos los tipos de acciones de la App.
+ */
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET     = "RESET";
+
+/**
+ * [ENGLISH]
+ * Reducer: A pure function that handles state changes based on actions.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Reducer: Una función pura que gestiona los cambios en el estado en función de las acciones.
+ */
+function counterReducer(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                counter: state.counter + 1,
+            };
+        case DECREMENT:
+            return {
+                ...state,
+                counter: state.counter - 1,
+            };
+        case RESET:
+            return {
+                ...state,
+                counter: 0,
+            };
+        default:
+            return state;
+    }
+}
+
+/**
+ * [ENGLISH]
+ * Create the 'Store'.
+ * 
+ * =========
+ * 
+ * [SPANISH]
+ * Creamos la 'Store'.
+ */
+const store = Redux.createStore(counterReducer);
+
+export default store;
